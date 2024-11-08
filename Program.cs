4cae617f475bb84df044d9ec5a13c985c46d605d
RubyWeng <ruby.weng777@gmail.com>
2024-11-09 06:54:54 +0800

    Initial commit for my first C# project

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿namespace SimpleCalculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double num1, num2, result;
+        string operation;
+
+        Console.WriteLine("Welcome to Calculation!");
+        Console.WriteLine("Please input 2 num and 1 operation");
+
+        Console.WriteLine("The first number :");
+        while (!double.TryParse(Console.ReadLine(), out num1))
+        {
+            Console.WriteLine("Invalid!");
+        }
+
+        Console.WriteLine("The operation is :");
+        operation = Console.ReadLine();
+
+        Console.WriteLine("The second number :");
+        while (!double.TryParse(Console.ReadLine(), out num2))
+        {
+            Console.WriteLine("Invalid!");
+        }
+
+        switch (operation)
+        {
+
+            case "+":
+                result = num1 + num2;
+                Console.WriteLine(num1 + " " + operation + " " + num2 + " = " + result);
+                break;
+            case "-":
+                result = num1 - num2;
+                Console.WriteLine(num1 + " " + operation + " " + num2 + " = " + result);
+                break;
+            case "*":
+                result = num1 * num2;
+                Console.WriteLine(num1 + " " + operation + " " + num2 + " = " + result);
+                break;
+            case "/":
+                if (num2 == 0)
+                {
+                    Console.Write("Divisor Can't set 0 !");
+                }
+                else
+                {
+                    result = num1 / num2;
+                    Console.WriteLine(num1 + " " + operation + " " + num2 + " = " + result);
+                }
+
+                break;
+            default:
+                Console.WriteLine("Invalid!");
+                break;
+        }
+
+    }
+}
